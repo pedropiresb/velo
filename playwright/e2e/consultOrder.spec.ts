4ed64f867ce6944de2844cd4e46e4169cf9f5624
Pedro Pires <pedropiresb@gmail.com>
2026-05-19 21:55:57 -0300

    exercicio 1

--- playwright/e2e/consultOrder.spec.ts
+++ playwright/e2e/consultOrder.spec.ts
@@ -4,4 +4,20 @@ test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible()
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+  await expect(page.getByText('Digite o número do seu pedido')).toBeVisible();
+  await expect(page.getByTestId('search-order-id')).toBeEmpty();
+ 
+  await page.getByTestId('search-order-id').fill('VLO-PSVJPK');
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  await expect(page.locator('xpath=//p[text()="VLO-PSVJPK"]')).toBeVisible();
+  await expect(page.getByText('Aprovado')).toBeVisible();
+  
+  //await expect(page.getByTestId('order-result-id')).toBeVisible();
+  //await page.getByTestId('order-result-status').toBeVisible();
+  //await expect(page.getByTestId('order-result-id')).toContainText('VLO-PSVJPK');
+  //await page.getByTestId('order-result-status').toContainText('Aprovado');
+
 });
--- src/pages/OrderLookup.tsx
+++ src/pages/OrderLookup.tsx
@@ -96,7 +96,7 @@ const OrderLookup = () => {
               </div>
               <Button
                 type="submit"
-                data-testid="search-order-button"
+                //data-testid="search-order-button"
                 className="w-full"
                 disabled={!orderId.trim() || isLoading}
               >
@@ -133,20 +133,20 @@ const OrderLookup = () => {
 
         {/* Order Result */}
         {searchedOrder && (
-          <Card className="animate-fade-in" data-testid={`order-result-${searchedOrder.id}`}>
+          <Card className="animate-fade-in" >
             <CardHeader>
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-3">
                   <Package className="w-5 h-5 text-muted-foreground" />
                   <div>
                     <p className="text-sm text-muted-foreground">Pedido</p>
-                    <p className="font-mono font-medium" data-testid="order-result-id">
+                    <p className="font-mono font-medium" >
                       {searchedOrder.id}
                     </p>
                   </div>
                 </div>
                 <div
-                  data-testid="order-result-status"
+                  //data-testid="order-result-status"
                   className={`flex items-center gap-2 px-4 py-2 rounded-full text-sm font-medium ${
                     searchedOrder.status === 'APROVADO'
                       ? 'bg-green-100 text-green-700'
