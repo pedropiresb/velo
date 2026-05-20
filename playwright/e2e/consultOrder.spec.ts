cfd330254b60ddf9cc5ddeb76963944870abde58
Pedro Pires <pedropiresb@gmail.com>
2026-05-20 18:30:37 -0300

    new test and support folder 2

--- playwright/e2e/consultOrder.spec.ts
+++ playwright/e2e/consultOrder.spec.ts
@@ -14,22 +14,22 @@ test('Search order', async ({ page }) => {
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
   await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
-  
+
   //act
 
   await expect(page.getByText('Digite o número do seu pedido')).toBeVisible();
   await expect(page.getByTestId('search-order-id')).toBeEmpty();
- 
+
   await page.getByTestId('search-order-id').fill(order);
   await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   //assert
 
   const orderCode = page.locator('xpath=//p[text()="Pedido"]/..//p[text()="VLO-PSVJPK"]')
-  await expect(orderCode).toBeVisible({timeout: 10000});
+  await expect(orderCode).toBeVisible({ timeout: 10000 });
   await expect(orderCode).toContainText(order);
   await expect(page.getByText(orderStatus)).toBeVisible();
-  
+
 });
 
 test('Order not found', async ({ page }) => {
@@ -45,15 +45,15 @@ test('Order not found', async ({ page }) => {
 
   await expect(page.getByText('Digite o número do seu pedido')).toBeVisible();
   await expect(page.getByTestId('search-order-id')).toBeEmpty();
- 
+
   await page.getByTestId('search-order-id').fill(order);
   await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   //assert
 
   const notFound = page.getByRole('heading', { name: 'Pedido não encontrado' })
-  await expect(notFound).toBeVisible({timeout: 10000});
+  await expect(notFound).toBeVisible({ timeout: 10000 });
   const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
-  await expect(message).toBeVisible({timeout: 10000}); 
+  await expect(message).toBeVisible({ timeout: 10000 });
 
 })
